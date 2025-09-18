69e516b8d0530336e96d46664a718c1e821442fc
Pedro Mesquita <p183998@dac.unicamp.br>
2025-09-18 20:20:42 -0300

    Add IndexedSet

---
+++ content/data-structures/IndexedSet.h
@@ -0,0 +1,16 @@
+/**
+ * Author: Pedro Mesquita
+ * Date: 2025-09-18
+ * License: CC0
+ * Source: kactl, Competitive Programmer’s Handbook
+ * Description: Similar to OrderStatisticTree,
+ * but uses a different library which may be more portable.
+ */
+#pragma once
+
+#include <ext/pb_ds/assoc_container.hpp> /** keep-include */
+using namespace __gnu_pbds;
+
+template<class T>
+using IndexedSet = tree<T, null_type, less<T>, rb_tree_tag,
+	tree_order_statistics_node_update>;
